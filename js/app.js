4f593fd4d117c8d12044bd3bb831bcc0d616060c
zechariahburchett <zechariah.burchett@gmail.com>
2018-06-06 12:45:35 -0400

    Add event listener to cards

--- js/app.js
+++ js/app.js
@@ -12,19 +12,21 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
+//add event listener on to deck for the cards
+deck.addEventListener('click', clickedCard);
 
 /*
  * set up the event listener for a card. If a card is clicked:
